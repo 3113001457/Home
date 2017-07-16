1928c064688317070d2dad7bd390e73ccf060abf
Z <Z>
2017-07-16 20:03:50 +0800

    Signed-off-by: Z <Z>

--- src/Template.js
+++ src/Template.js
@@ -1,38 +1,7 @@
 import React from 'react'
-import Meal from './Meal'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
 
-const Template = ({ match }) => (
-  <Router>
-  <div>
-    <h2>Template</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props v. State
-        </Link>
-      </li>
-    </ul>
-    <Route path={`${match.url}/:rendering`} component={Meal}/>
-    <Route exact path={match.url} render={() => (
-      <h3>Please select a topic.</h3>
-    )}/>
-  </div>
-  </Router>
-)
+const Template = () => (
+  <div>213</div>
+);
 
 export default Template;
